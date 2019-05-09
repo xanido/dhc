5776da346dbdcf0e2783037e2ff7177f3ea91840
Timothy Anido <xanido@gmail.com>
2019-05-09 17:14:28 +1000

    Add hCard microformat markup

--- src/components/HCPreview.js
+++ src/components/HCPreview.js
@@ -1,17 +1,39 @@
 import React from 'react'
 
-const HCPreview = ({ fields, values }) => (
-  <div>
-    {fields.map(field => (
-      <div key={field.name}>
-        <h5>{field.label}</h5>
-        <p data-testid={`preview-${field.name}`}>{values[field.name]}</p>
+const createFieldValueGetter = (values) =>
+  (fieldName) => values[fieldName]
+
+const HCPreview = ({ fields, values }) => {
+  const getValue = createFieldValueGetter(values)
+  return (
+    <div className='vcard'>
+      <div className='fn' title='Full name'>{getValue('givenName')} {getValue('surname')}</div>
+      {getValue('avatar') &&
+        <img src={getValue('avatar')} alt='Avatar preview' />
+      }
+      <div className='email'>{getValue('email')}</div>
+      <div className='tel'>{getValue('phone')}</div>
+      <div className='adr'>
+        <div>
+          <span className='street-address'>
+            {getValue('addressHouseNumber')}
+            {' '}
+            {getValue('addressStreet')}
+          </span>
+        </div>
+        <div>
+          <span className='locality'>{getValue('addressSuburb')}</span>
+          {getValue('addressState') ? ', ' : ''}
+          <span className='region'>{getValue('addressState')}</span>
+        </div>
+        <div>
+          <span className='postal-code'>{getValue('addressPostcode')}</span>
+          {' '}
+          <span className='country-name'>{getValue('addressCountry')}</span>
+        </div>
       </div>
-    ))}
-    <div>
-      <img src={values['avatar']} data-testid='preview-avatar' />
     </div>
-  </div>
-)
+  )
+}
 
 export default HCPreview
--- src/tests/integration.test.js
+++ src/tests/integration.test.js
@@ -6,30 +6,33 @@ import HCApp from '../containers/HCApp'
 afterEach(cleanup)
 
 test('HCPreview to be blank on initial render', () => {
-  const { queryByTestId } = render(
+  const { queryByTitle } = render(
     <HCApp />
   )
 
-  expect(queryByTestId(/preview-givenName/i).textContent).toBeFalsy()
+  expect(queryByTitle(/Full name/i).textContent.trim()).toBeFalsy()
 })
 
 test('HCPreview is updated as fields are changed', () => {
-  const testVal = 'John'
-  const { queryByLabelText, getByLabelText, queryByTestId } = render(
+  const expectedGivenName = 'John'
+  const expectedSurname = 'Appleseed'
+  const { getByLabelText, queryByTitle } = render(
     <HCApp />
   )
 
-  expect(queryByLabelText(/Given Name/i)).toBeTruthy()
-
   fireEvent.change(getByLabelText(/Given Name/i), { target: {
-    value: testVal
+    value: expectedGivenName
   } })
 
-  expect(queryByTestId(/preview-givenName/i).textContent).toEqual(testVal)
+  fireEvent.change(getByLabelText(/Surname/i), { target: {
+    value: expectedSurname
+  } })
+
+  expect(queryByTitle(/Full name/i).textContent).toEqual(expectedGivenName + ' ' + expectedSurname)
 })
 
 test('HCPreview avatar preview is updated when file selected', async () => {
-  const { getByLabelText, getByTestId } = render(
+  const { getByLabelText, getByAltText } = render(
     <HCApp />
   )
 
@@ -38,7 +41,7 @@ test('HCPreview avatar preview is updated when file selected', async () => {
   fireEvent.change(imageInput, { target: { files: [file] } })
 
   await wait(() => {
-    const avatarPreview = getByTestId('preview-avatar')
+    const avatarPreview = getByAltText(/Avatar preview/i)
     expect(avatarPreview.src).toEqual(
       'data:image/jpg;base64,KOODhCk='
     )
